1c1bbe4ef51056f21d61ec682fa8063bb3bc4520
kkazmier <dev.karol.kazmierczak@gmail.com>
2025-04-29 20:16:37 +0200

    Initial commit

---
+++ src/main/java/pl/gastro/gastro_management_suite/GastroManagementSuiteApplication.java
@@ -0,0 +1,13 @@
+package pl.gastro.gastro_management_suite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastroManagementSuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastroManagementSuiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/gastro/gastro_management_suite/GastroManagementSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gastro.gastro_management_suite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastroManagementSuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
