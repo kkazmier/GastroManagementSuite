3a122bc3816ef674597ae97d9c94d85a04be213c
kkazmier <dev.karol.kazmierczak@gmail.com>
2025-05-01 16:05:59 +0200

    Add allWaiters().

--- src/main/java/pl/gastro/gastro_management_suite/controller/WaiterController.java
+++ src/main/java/pl/gastro/gastro_management_suite/controller/WaiterController.java
@@ -1,4 +1,20 @@
 package pl.gastro.gastro_management_suite.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.gastro.gastro_management_suite.model.Waiter;
+import pl.gastro.gastro_management_suite.service.WaiterServiceImpl;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
 public class WaiterController {
+    private final WaiterServiceImpl waiterService;
+
+    @GetMapping("/employees")
+    public List<Waiter> allWaiters() {
+        return waiterService.allWaiters();
+    }
 }
