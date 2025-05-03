884eff8bd8fc10ef99189f7d19d12966ebfca36d
kkazmier <dev.karol.kazmierczak@gmail.com>
2025-05-03 14:37:39 +0200

    Security config.

--- src/main/java/pl/gastro/gastro_management_suite/GastroManagementSuiteApplication.java
+++ src/main/java/pl/gastro/gastro_management_suite/GastroManagementSuiteApplication.java
@@ -2,6 +2,8 @@ package pl.gastro.gastro_management_suite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import pl.gastro.gastro_management_suite.config.SecurityConfig;
 
 @SpringBootApplication
 public class GastroManagementSuiteApplication {
