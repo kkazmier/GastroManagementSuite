b335002a3a37c320f99f58eca105a0701a63c4fa
kkazmier <dev.karol.kazmierczak@gmail.com>
2025-05-04 20:16:17 +0200

    Clean code.

--- src/main/java/pl/gastro/gastro_management_suite/GastroManagementSuiteApplication.java
+++ src/main/java/pl/gastro/gastro_management_suite/GastroManagementSuiteApplication.java
@@ -2,8 +2,6 @@ package pl.gastro.gastro_management_suite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import pl.gastro.gastro_management_suite.config.SecurityConfig;
 
 @SpringBootApplication
 public class GastroManagementSuiteApplication {
